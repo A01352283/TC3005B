c5b3829fa1934dec1fc205e5e2d8a75a7d479597
A01352283 <a01352283@tec.mx>
2023-04-13 18:53:39 -0600

    Initial Scrapy setup

---
+++ tareaScraping/tareaScraping/pipelines.py
@@ -0,0 +1,26 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class TareascrapingPipeline:
+    def process_item(self, item, spider):
+        return item
+
+import json
+
+class CarsPipeline:
+    def process_item(self, item, spider):
+        # Convert item to dictionary
+        item_dict = dict(item)
+
+        # Process and store data
+        with open('output.json', 'a') as f:
+            f.write(json.dumps(item_dict) + '\n')
+
+        return item
---
+++ tareaScraping/tareaScraping/spiders/kavakSpider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class KavakspiderSpider(scrapy.Spider):
+    name = "kavakSpider"
+    allowed_domains = ["www.kavak.com"]
+    start_urls = ["https://www.kavak.com/mx/seminuevos"]
+        
+    def parse(self, response):
+        # Extract links to product pages
+        # Extract the link from the <a> tag with class "card"
+        product_links = response.css('.card::attr(href)').extract()
+
+        # Follow each product link and parse the product page
+        for product_link in product_links:
+            yield response.follow(product_link, callback=self.parse_product)
+
+    def parse_product(self, response):
+        # Extract data from the product page
+        data = {}
+        data['title'] = response.css('.product-title::text').get()
+        data['price'] = response.css('.product-price::text').get()
+        data['litres'] = response.css('.feature:contains("Litres") + .feature-value::text').get()
+        data['transmission'] = response.css('.feature:contains("Transmission") + .feature-value::text').get()
+
+        yield data
