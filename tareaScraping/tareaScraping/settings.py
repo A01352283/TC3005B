b96c2d23e791ebb9d5c0ab873f27eee3c0dc2f79
A01352283 <a01352283@tec.mx>
2023-04-29 02:48:06 -0600

    Configurado para hacer scrape completo

--- tareaScraping/tareaScraping/settings.py
+++ tareaScraping/tareaScraping/settings.py
@@ -12,11 +12,17 @@ BOT_NAME = "tareaScraping"
 SPIDER_MODULES = ["tareaScraping.spiders"]
 NEWSPIDER_MODULE = "tareaScraping.spiders"
 
-ITEM_PIPELINES = {
+# JsonPipeline settings
+""" ITEM_PIPELINES = {
     'tareaScraping.pipelines.JsonPipeline': 300,
 }
-FEED_FORMAT = 'json'
 FEED_URI = 'output.json'
+FEED_FORMAT = 'json' """
+
+# CsvPipeline settings
+FEEDS = {
+    'scrapedResults.csv': {'format': 'csv', 'overwrite': False}
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "tareaScraping (+http://www.yourdomain.com)"
@@ -34,12 +40,12 @@ DOWNLOAD_HANDLERS = {
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -82,16 +88,16 @@ TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 0.5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 5
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- tareaScraping/tareaScraping/spiders/kavakSpider.py
+++ tareaScraping/tareaScraping/spiders/kavakSpider.py
@@ -1,64 +1,91 @@
-# Output format: data = {'urls': product_links}
 import scrapy
+from scrapy_playwright.page import PageMethod
+
+# scrapy crawl kavakSpider -s JOBDIR=crawls/kavakcrawl  
 
 class KavakspiderSpider(scrapy.Spider):
     name = "kavakSpider"
+
+    # To disable headless mode
+    """ custom_settings = {
+        "PLAYWRIGHT_BROWSER_TYPE": "chromium",
+        "PLAYWRIGHT_LAUNCH_OPTIONS": {
+            "headless": False,
+        },
+        "PLAYWRIGHT_CONTEXT_OPTIONS": {
+            "ignoreHTTPSErrors": True,
+            "viewport": {
+                "width": 1920,
+                "height": 1080,
+            },
+        }
+    } """
     
     def start_requests(self):
-        url = "https://www.kavak.com/mx/seminuevos/?page=0"
-        yield scrapy.Request(url, 
-                            meta=dict(playwright = True,
-                                      playwright_include_page = True,
-                                      errback=self.errback,
-                                      playwright_page_methods={"click": PageMethod("click", selector="a")}
-                            ))
-        # To route our requests through 
-        #scrapy-playwright we just need to enable it in the Request meta dictionary 
-        #by setting meta={'playwright': True}.
-
+        for i in range(0, 381):
+            url = "https://www.kavak.com/mx/seminuevos/?page=" + str(i)
+            yield scrapy.Request(url, callback=self.parse, meta={"playwright": True})
+            
+        """ url = "https://www.kavak.com/mx/seminuevos/?page=0"
+        yield scrapy.Request(url, callback=self.parse, meta={"playwright": True}) """  
+    
+    # Get the urls of each car page from the preview cards
     def parse(self, response):
-        
 
-        yield scrapy.Request('https://www.kavak.com/mx/usado/jeep-renegade-sport-suv-2017?id=244926'
-                             , callback=self.parse_page, meta={'playwright': True})
+        # Get the urls until there are 30 urls
+        urls = response.css('a.card::attr(href)').getall()
 
-        """ urls = response.css('a.card::attr(href)').getall()
-
+        # Visit each car page
         for url in urls:
-            yield scrapy.Request(url, callback=self.parse_page, meta={'playwright': True}) """
+            yield scrapy.Request(url, 
+                                callback=self.parse_page, 
+                                meta={"playwright": True,
+                                    "playwright_page_methods": [PageMethod("click", "h3.accordion-label")]
+                                    }
+                                )
+        # To route our requests through scrapy-playwright we just need to enable it in the Request meta dictionary 
+        # by setting meta={'playwright' True}.
 
 
+    # Scrape the data from each car page
     async def parse_page(self, response):
-
         # Get the text of a h1 with the class "title"
-        # Translate the previous comment to spanish
         carPathSpan = response.css("span.path::text").getall()
-        brand = carPathSpan[1]
-        model = carPathSpan[2]
-        #yearModel = carPathSpan[3]
+        marca = carPathSpan[1]
+        modelo = carPathSpan[2]
 
-        # Get the text of a <p> with the class "value" which is inside a <div> with the class 
-        # "filter-container" if the other <p> inside the <div> has the text "Año"
-        year = response.css('div.filter-container:contains("Año") p.value::text').get()
+        # Get the text of a <p> with the class "value" which is inside a <div> with the class "filter-container" if the 
+        # other <p> inside the <div> has the text "Año"
+        año = response.css('div.filter-container:contains("Año") p.value::text').get()
         
-        # Get the price of the car
-        price = response.css("span.normal::text").getall()
+        # Get the precio of the car
+        precio = response.css("span.normal::text").getall()
 
-        # Get the text of a <p> with the class "description" which is inside a <div> with the class 
-        # "feature-content" if the other <p> inside the <div> has the text "Transmisión"
-        transmission = response.css('div.feature-content:contains("Transmisión") p.description::text').get()
-
-        # Get the text of a <p> with the class "description" which is inside a <div> with the class 
-        # "feature-content" if the other <p> inside the <div> has the text "Combustible"
-        #combustible = response.css('div.feature-content:contains("Combustible") p.description::text').get()
-
-        # Get the text of a <p> with the class "description" which is inside a <div> with the class 
-        # "feature-content" if the other <p> inside the <div> exavtly matches the text "Combustible"
-        #litros = response.css('div.feature-content:contains("Combustible") p.description::text').get()
+        # Get the text of a <p> with the class "description" which is inside a <div> with the class "feature-content" if
+        # the other <p> inside the <div> has the text "Transmisión"
+        transmisión = response.css('div.feature-content:contains("Transmisión") p.description::text').get()
         
-        # Get the text of a <p> with the class "description" which is inside a <div> with the class 
-        # "feature-content" if the other <p> inside the <div> has the text "Litros"
+        # Get the text of a <p> with the class "description" which is inside a <div> with the class "feature-content" if
+        # the other <p> inside the <div> has the text "Combustible"
+        divsCombustible = response.css('div.feature-content:contains("Combustible") p.description::text').getall()
+        rendimiento = divsCombustible[0] 
+        combustible = ""
+        try:
+            combustible = divsCombustible[1]
+        except:
+            combustible = None       
+        
+        # Get the text of a <p> with the class "description" which is inside a <div> with the class "feature-content" if
+        # the other <p> inside the <div> has the text "Litros"
         litros = response.css('div.feature-content:contains("Litros") p.description::text').get()
 
-        yield {'brand': brand, 'model': model, 'year': year, 
-               'price': price, 'transmission': transmission}
+        #tipo_vehiculo = response.css('div.feature-content:contains("Tipo de Carrocería") p.description::text').get()
+
+
+        yield {'marca': marca, 'modelo': modelo, 'año': año, 
+                'precio': precio, 'transmisión': transmisión, 'rendimiento': rendimiento,
+                'combustible': combustible, 'litros': litros}
+
+    async def errback_close_page(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
