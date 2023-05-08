50b1e88a74cb1149ba9c17936afe4f476bcfd71d
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2023-05-08 00:53:01 -0600

    Actualiza el schema de MySQL
    Actualiza el schema de MySQL, desnormalizando el diagrama de Mongo

--- schemas/MySQL/swivel-schema.sql
+++ schemas/MySQL/swivel-schema.sql
@@ -4,102 +4,113 @@ USE swivelDB;
 
 CREATE TABLE usuario_final(
     usuario_final_id INT NOT NULL AUTO_INCREMENT,
-    nombres VARCHAR(254) NOT NULL,
-    apellidos VARCHAR(254) NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    numero_telefonico VARCHAR(50),
-    tipo_usuario CHAR(50) NOT NULL,
-    lista_deseos_id INT REFERENCES lista_deseos(lista_deseos_id),
-    dirección_id INT REFERENCES dirección(dirección_id),
+    datos_usuario_id INT NOT NULL REFERENCES datos_usuario(datos_usuario_id),
+    dirección_id INT NOT NULL REFERENCES dirección(dirección_id),
     PRIMARY KEY (usuario_final_id)
 );
 
 CREATE TABLE vendedor(
     vendedor_id INT NOT NULL AUTO_INCREMENT,
-    nombres VARCHAR(254) NOT NULL,
-    apellidos VARCHAR(254) NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    numero_telefonico VARCHAR(50),
-    tipo_usuario CHAR(50) NOT NULL,
-    gerente_id INT REFERENCES gerente(gerente_id),
+    datos_usuario_id INT NOT NULL REFERENCES datos_usuario(datos_usuario_id),
+    contar_ventas_en_proceso INT NOT NULL,
+    contar_ventas_completadas INT NOT NULL,
+    gerente_id INT NOT NULL REFERENCES gerente(gerente_id),
     PRIMARY KEY (vendedor_id)
 );
 
 CREATE TABLE gerente(
     gerente_id INT NOT NULL AUTO_INCREMENT,
-    nombres VARCHAR(254) NOT NULL,
-    apellidos VARCHAR(254) NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    numero_telefonico VARCHAR(50),
-    tipo_usuario CHAR(50) NOT NULL,
-    agencia_id INT REFERENCES agencia(agencia_id),
+    datos_usuario_id INT NOT NULL REFERENCES datos_usuario(datos_usuario_id),
+    agencia_id INT NOT NULL REFERENCES agencia(agencia_id),
     PRIMARY KEY (gerente_id)
 );
 
 CREATE TABLE agencia(
     agencia_id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(254) NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    tipo_usuario CHAR(50) NOT NULL,
-    grupo_automotriz_id INT REFERENCES grupo_automotriz(grupo_automotriz_id),
-    dirección_id INT REFERENCES dirección(dirección_id),
+    dirección_id INT NOT NULL REFERENCES dirección(dirección_id),
+    datos_lugar_id INT NOT NULL REFERENCES datos_lugar(datos_lugar_id),
+    grupo_automotriz_id INT NOT NULL REFERENCES grupo_automotriz(grupo_automotriz_id),
     PRIMARY KEY (agencia_id)
 );
 
 CREATE TABLE grupo_automotriz(
     grupo_automotriz_id INT NOT NULL AUTO_INCREMENT,
-    nombre_grupo_automotriz VARCHAR(254) NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    tipo_usuario CHAR(50) NOT NULL,
-    dirección_id INT REFERENCES dirección(dirección_id),
+    datos_lugar_id INT NOT NULL REFERENCES datos_lugar(datos_lugar_id),
+    dirección_id INT NOT NULL REFERENCES dirección(dirección_id),
     PRIMARY KEY (grupo_automotriz_id)
 );
 
 CREATE TABLE superadmin(
-    admin_id INT NOT NULL AUTO_INCREMENT,
-    nombres CHAR(50) NOT NULL,
-    apellidos CHAR(50) NOT NULL,
+    superadmin_id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(254) NOT NULL,
+    apellidos VARCHAR(254) NOT NULL,
     email VARCHAR(254) NOT NULL,
-    contraseña VARCHAR(30) NOT NULL,
-    tipo_usuario CHAR(50) NOT NULL,
+    contraseña VARCHAR(254) NOT NULL,
     PRIMARY KEY (admin_id)
 );
 
 CREATE TABLE dirección(
     dirección_id INT NOT NULL AUTO_INCREMENT,
     calle VARCHAR(254) NOT NULL,
-    número_exterior VARCHAR(10) NOT NULL,
-    ciudad VARCHAR(60) NOT NULL,
-    estado VARCHAR(254) NOT NULL,
-    país VARCHAR(254) NOT NULL,
-    código_postal VARCHAR(5) NOT NULL,
+    número_interior VARCHAR(15) NOT NULL,
+    número_exterior VARCHAR(15) NOT NULL,
+    ciudad VARCHAR(32) NOT NULL,
+    estado VARCHAR(32) NOT NULL,
+    país VARCHAR(64) NOT NULL,
+    código_postal CHAR(5) NOT NULL,
     PRIMARY KEY (dirección_id)
 );
 
 CREATE TABLE lista_deseos(
-    lista_deseos_id INT NOT NULL AUTO_INCREMENT,
-    -- Array of car ids
-    PRIMARY KEY (lista_deseos_id)
+    user_id INT NOT NULL REFERENCES usuario_final(usuario_final_id),
+    auto_id INT NOT NULL REFERENCES automovil(auto_id),
+    PRIMARY KEY (user_id, auto_id)
+);
+
+CREATE TABLE datos_usuario(
+    datos_usuario_id INT NOT NULL AUTO_INCREMENT,
+    nombres VARCHAR(254) NOT NULL,
+    apellidos VARCHAR(254) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    contraseña VARCHAR(254) NOT NULL,
+    número_telefónico CHAR(10),
+    is_account_verified BOOLEAN NOT NULL,
+    email_verification_token CHAR(32),
+    account_provider CHAR(254),
+    PRIMARY KEY (datos_usuario_id)
+);
+
+CREATE TABLE datos_lugar(
+    datos_lugar_id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(254) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    contraseña VARCHAR(254) NOT NULL,
+    número_telefónico CHAR(10) NOT NULL,
+    PRIMARY KEY (datos_lugar_id)
+);
+
+CREATE TABLE documentos(
+    documentos_id INT NOT NULL AUTO_INCREMENT,
+    usuario_final_id INT NOT NULL REFERENCES usuario_final(usuario_final_id),
+    nombre_documento VARCHAR(254) NOT NULL,
+    url_documento VARCHAR(65532) NOT NULL,
+    PRIMARY KEY (documentos_id)
 );
 
 CREATE TABLE automovil(
     auto_id INT NOT NULL AUTO_INCREMENT,
-    marca VARCHAR(254) NOT NULL,
+    agencia_id INT NOT NULL REFERENCES agencia(agencia_id),
+    marca VARCHAR(32) NOT NULL,
     modelo VARCHAR(254) NOT NULL,
-    año VARCHAR(4) NOT NULL,
+    año INT NOT NULL,
     precio INT NOT NULL,
     color VARCHAR(254) NOT NULL,
-    combustible CHAR (75) NOT NULL,
-    litros INT NOT NULL,
-    motor VARCHAR (75),
-    transmisión CHAR (75) NOT NULL,
-    tipo_vehículo CHAR (75) NOT NULL,
-    descripción VARCHAR (2000),
-    agencia_id INT REFERENCES agencia(agencia_id),
+    combustible VARCHAR (10) NOT NULL,
+    autonomía INT NOT NULL,
+    transmisión VARCHAR (10) NOT NULL,
+    cantidad INT NOT NULL,
+    motor VARCHAR (15) NOT NULL,
+    tipo_vehículo VARCHAR (254) NOT NULL,
+    descripción TEXT NOT NULL,
     PRIMARY KEY (auto_id)
     )
